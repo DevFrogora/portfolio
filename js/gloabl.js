7eda67fda640391e16ea2d0ac0f82e951ba59ab7
DevFrogora <rr4428310@gmail.com>
2022-10-14 05:07:59 +0100

    Auth For Admin

--- js/gloabl.js
+++ js/gloabl.js
@@ -18,15 +18,16 @@ class urldb {
 }
 let localHost = "https://localhost:5001";
 let azureHost = "https://portfoliowebapi.azurewebsites.net";
-let webapi_domain=localHost;  //
+let webapi_domain=azureHost;  //
 window.urllist = new urldb(webapi_domain);
 
+let resume = await Get(window.urllist.webapiDomain+"/resume/", null);
 let project= await Get(window.urllist.webapiDomain+"/project/",null);
 let contact = await Get(window.urllist.webapiDomain+"/contact/", null);
-let resume = await Get(window.urllist.webapiDomain+"/resume/", null);
 window.db = new db(project,contact,resume);
 
 // window.contact = contact;
 // console.log(window.db.project);
 
 // console.log("hello");
+console.log(window.db.project);
